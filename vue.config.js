dda49301e255f28ecb4db9cce05f780526a95d68
dmeritano <dmeritano@hotmail.com>
2025-04-24 14:38:50 -0300

    Agregue configuracion en vue.config.js para hacer proxy en entorno de desarrollos (devServer) y no tener problemas de CORS

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV  ===  'production'  ?  '.'  :  '/',
+  publicPath: process.env.NODE_ENV === "production" ? "." : "/",
+  devServer: {
+    proxy: {
+      "^/DmsRestServiceDTOP": {
+        target: "http://localhost/",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
   pluginOptions: {
     i18n: {
-      locale: 'es',
-      fallbackLocale: 'es',
-      localeDir: 'locales',
+      locale: "es",
+      fallbackLocale: "es",
+      localeDir: "locales",
       enableLegacy: false,
       runtimeOnly: false,
       compositionOnly: false,
-      fullInstall: true
-    }
-  }
+      fullInstall: true,
+    },
+  },
 })
